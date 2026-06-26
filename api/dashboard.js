8918fc8f8293047ce688454d076d152afdfab1d6
ecky16 <eckyrahmad769@gmail.com>
2026-06-27 06:02:20 +0700

    Update dashboard.js

--- api/dashboard.js
+++ api/dashboard.js
@@ -20,19 +20,4 @@ module.exports = async (req, res) => {
     res.status(500).json({ error: err.toString() });
   }
   
-// Tambahkan di dashboard.txt
-module.exports = async (req, res) => {
-  const GAS_WEBAPP_URL = process.env.GAS_WEBAPP_URL; 
-  try {
-    const resp = await fetch(GAS_WEBAPP_URL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ action: "get_produktivitas" })
-    });
-    const result = await resp.json();
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).json(result.data);
-  } catch (err) {
-    res.status(500).json({ error: err.toString() });
-  }
 };
